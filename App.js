7817c05b77103b2ffd4c7aecae15b3cc4f5df215
1wflores <114186244+1wflores@users.noreply.github.com>
2024-02-25 13:38:29 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NFT from './NFT';
+
+function App() {
+  return (
+    <div className="App">
+      <NFT />
+    </div>
+  );
+}
+
+export default App;
