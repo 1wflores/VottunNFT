e08bd5ccb29119d64b0cfc256d37e5e52113a3fa
1wflores <114186244+1wflores@users.noreply.github.com>
2024-02-26 14:02:23 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NFT from './NFT';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function NFT() {
+  const [nftData, setNftData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.vottun.io/erc1155/contractAddress') // replace contractAddress with actual one
+      .then(response => {
+        setNftData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <NFT />
+    <div>
+      {nftData.map((nft, index) => (
+        <div key={index}>
+          <h2>{nft.name}</h2>
+          <img src={nft.image} alt={nft.name} />
+          <p>{nft.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default NFT;
